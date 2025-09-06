d8cf67fd42414b1d2c792385139b69536b2cd3be
Robert Sousa <robertsousa4@gmail.com>
2025-09-06 08:55:22 -0400

    Update

--- components/navbar.py
+++ components/navbar.py
@@ -1,14 +1,16 @@
-from dash import html, dcc
 import dash_bootstrap_components as dbc
+from dash import dcc, html
 
 
 def create_navbar():
     return dbc.NavbarSimple(
         children=[
             dbc.NavItem(dcc.Link("Home", href="/", className="nav-link")),
-            dbc.NavItem(dcc.Link("Transactions", href="/transactions", className="nav-link"))
+            dbc.NavItem(
+                dcc.Link("Transactions", href="/transactions", className="nav-link")
+            ),
         ],
         brand="My Dashboard",
         color="dark",
-        dark=True
+        dark=True,
     )
